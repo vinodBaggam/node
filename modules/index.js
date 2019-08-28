fdf8c7a1dcf2698f288537534348c82b889ef2be
vinodbaggam <baggamvinod@gmail.com>
2019-08-28 12:02:32 +0530

    Exporting Modules

---
+++ modules/index.js
@@ -0,0 +1,7 @@
+const file1 =  require("./file1");
+const utils =  require("./utils");
+console.log(file1.myname1);
+console.log(file1.myname2);
+
+console.log(utils.add(10,2));
+console.log(utils.sub);
---
+++ modules/utils.js
@@ -0,0 +1,4 @@
+module.exports.add = (a,b) =>  "Sum is"+(a+b);
+module.exports.sub = (a,b) =>  "Sub is"+(a-b);
+module.exports.mul = (a,b) =>  "Mlu is"+(a*b);
+module.exports.devide = (a,b) =>  "Division is"+(a/b);
